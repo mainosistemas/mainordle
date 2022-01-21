ef852546a5d4ea0be996b0c17fa40b26d4712130
Neil <me@neil.pro>
2022-01-21 12:21:39 -0300

    Validate that the word has 5 letters and only letters

--- lib/mainordle/word.rb
+++ lib/mainordle/word.rb
@@ -1,15 +1,28 @@
 module Mainordle
   class Word
+    ONLY_LETTERS = /\A[a-z]+\z/i
+
     def initialize(word)
       @word = word
-    end
 
-    def valid?
-      @word.size == 5
+      validate_word!
     end
 
     def letters
       @word.split(//)
     end
+
+    private
+
+    def validate_word!
+      if @word.size != 5
+        raise ArgumentError, "does not have 5 letters"
+      end
+
+      unless @word.match(ONLY_LETTERS)
+        raise ArgumentError, "has invalid characters"
+      end
+    end
+
   end
 end
--- spec/mainordle/word_spec.rb
+++ spec/mainordle/word_spec.rb
@@ -1,23 +1,51 @@
 require "spec_helper"
 
 describe Mainordle::Word do
-  describe "#valid?" do
+  describe "#initialize" do
     it "is valid with 5 letters" do
-      word = described_class.new("letra")
-
-      expect(word).to be_valid
+      expect do
+        described_class.new("letra")
+      end.not_to raise_error()
     end
 
-    it "is not valid with 4 letters" do
-      word = described_class.new("trem")
-
-      expect(word).not_to be_valid
+    context "when the word has 4 letters" do
+      it "raises an ArgumentError" do
+        expect do
+          described_class.new("trem")
+        end.to raise_error(ArgumentError, "does not have 5 letters")
+      end
     end
 
-    it "is not valid with 6 letters" do
-      word = described_class.new("musica")
+    context "when the word has 6 letters" do
+      it "raises an ArgumentError" do
+        expect do
+          described_class.new("musica")
+        end.to raise_error(ArgumentError, "does not have 5 letters")
+      end
+    end
 
-      expect(word).not_to be_valid
+    context "when the word has a star" do
+      it "raises an ArgumentError" do
+        expect do
+          described_class.new("vali*")
+        end.to raise_error(ArgumentError, "has invalid characters")
+      end
+    end
+
+    context "when the word has a dot" do
+      it "raises an ArgumentError" do
+        expect do
+          described_class.new("vali.")
+        end.to raise_error(ArgumentError, "has invalid characters")
+      end
+    end
+
+    context "when the word has a number" do
+      it "raises an ArgumentError" do
+        expect do
+          described_class.new("vali1")
+        end.to raise_error(ArgumentError, "has invalid characters")
+      end
     end
   end
 
